0de0acc42e42d0bf4fd27ce8def09360b0229421
bywhite0 <3143027430@qq.com>
2022-06-10 02:30:03 +0900

    fix: js-cookie CDN link

--- goindex-acrou/dist/app.js
+++ goindex-acrou/dist/app.js
@@ -1,6 +1,6 @@
 
           var scripts = ["https://fastly.jsdelivr.net/gh/alx-xlx/goindex/goindex-acrou/dist/js/app.da7f5f3f.js","https://fastly.jsdelivr.net/gh/alx-xlx/goindex/goindex-acrou/dist/js/chunk-vendors.3c185c97.js"];
-          var cdnjs = ["https://unpkg.com/vue@2.6.11/dist/vue.min.js","https://unpkg.com/vue-i18n@8.17.3/dist/vue-i18n.min.js","https://unpkg.com/vue-router@3.1.6/dist/vue-router.min.js","https://unpkg.com/vuex@3.4.0/dist/vuex.js","https://unpkg.com/axios@0.19.2/dist/axios.min.js","https://unpkg.com/element-ui@2.13.1/lib/index.js","https://unpkg.com/lodash@4.17.15/lodash.min.js","https://unpkg.com/js-cookie@2.2.1/src/js.cookie.min.js","https://unpkg.com/lowdb@1.0.0/dist/low.min.js","https://unpkg.com/lowdb@1.0.0/dist/LocalStorage.min.js","https://at.alicdn.com/t/font_1760192_8add3xcxs4u.js"];
+          var cdnjs = ["https://unpkg.com/vue@2.6.11/dist/vue.min.js","https://unpkg.com/vue-i18n@8.17.3/dist/vue-i18n.min.js","https://unpkg.com/vue-router@3.1.6/dist/vue-router.min.js","https://unpkg.com/vuex@3.4.0/dist/vuex.js","https://unpkg.com/axios@0.19.2/dist/axios.min.js","https://unpkg.com/element-ui@2.13.1/lib/index.js","https://unpkg.com/lodash@4.17.15/lodash.min.js","https://unpkg.com/js-cookie@2.2.1/src/js.cookie.js","https://unpkg.com/lowdb@1.0.0/dist/low.min.js","https://unpkg.com/lowdb@1.0.0/dist/LocalStorage.min.js","https://at.alicdn.com/t/font_1760192_8add3xcxs4u.js"];
           cdnjs.forEach((item) => {
             document.write('<script src="' + item + '"></script>');
           });
